c88470d8387df5c13b3046fc0f56cf3180e741cc
SalCoombs <sal@waldegrave.org>
2024-12-16 13:32:31 -0800

    Made the site way more awsome.

--- docs/index.js
+++
@@ -1,5 +0,0 @@
-let version = 5;
-document.getElementById("update-button").addEventListener("click", function () {
-  version += 1;
-  document.getElementById("update-version").innerHTML = "Update: 1." + version;
-});
