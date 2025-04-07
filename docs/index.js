df6ffe1bebf4f75f7882b4f89977e7bea1918758
SalCoombs <sal@waldegrave.org>
2025-04-07 11:03:42 -0700

    Solved flexbox bug and moved testfiles to archive folder

--- docs/index.js
+++ docs/index.js
@@ -1,19 +1,1 @@
-let hello = 1;
 
-for (i = 0; i < 10; i++) {
-  hello += i;
-}
-
-document.addEventListener(onclick, () => {
-  alert("Button clicked. Your number is: " + hello);
-});
-
-const game_input = document.getElementById("game-input");
-const game_text = document.getElementById("game-text");
-
-game_input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    game_text.textContent = game_input.value;
-    console.log("Hi");
-  }
-});
